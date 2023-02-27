91d25b8eb68a36ea00632018e0623fc703f7f967
Mariana Gimenez <gimenezmarianae@gmail.com>
2023-02-27 15:58:55 +0100

    typo in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :rents
   has_many :games
 
-  validate_uniqueness_of :email
-  validate_presence_of :email, :first_name, :last_name
+  validates_uniqueness_of :email
+  validates_presence_of :email, :first_name, :last_name
 end
