f4504000c093c409646c66bf863df07be3742868
Mariana Gimenez <gimenezmarianae@gmail.com>
2023-02-28 11:36:16 +0100

    updated user model and controller, and created rent model. add user seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_162244) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_094157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_162244) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "rents", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_rents_on_game_id"
+    t.index ["user_id"], name: "index_rents_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_162244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
+  add_foreign_key "rents", "games"
+  add_foreign_key "rents", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Cleaning Database'
 Game.destroy_all
+User.destroy_all
+
+puts 'Creating Users'
+ana = User.create(
+  email: 'ana@ana.com',
+  password: '12345678',
+  first_name: 'Ana',
+  last_name: 'Fa'
+)
+
+bob = User.create(
+  email: 'bob@bob.com',
+  password: '12345678',
+  first_name: 'Bob',
+  last_name: 'Na'
+)
+
+jon = User.create(
+  email: 'jon@snow.com',
+  password: '12345678',
+  first_name: 'Jon',
+  last_name: 'Snow'
+)
 
 puts "Creating 10 games..."
 Game.create(
@@ -14,69 +37,69 @@ Game.create(
   platform: 'Nintendo 64',
   picture: 'https://assets-prd.ignimgs.com/2021/12/07/zeldaoot-ignart-1638901927766.jpg',
   price: 5,
-  # user_id: rand(1..4)
+  user_id: ana.id
 )
 Game.create(
   name: "Tony Hawk's Pro Skater 2",
   platform: 'Playstation',
   picture: 'https://assets-prd.ignimgs.com/2021/12/17/tony-hawk-prp-skater-2-1639763193495.jpg',
   price: 5,
-  # user_id: rand(1..4)
+  user_id: ana.id
 )
 Game.create(
   name: "Grand Theft Auto IV",
   platform: 'Playstation 3',
   picture: 'https://assets-prd.ignimgs.com/2022/01/07/gta-4-button-char-v2-1641587906428.jpg',
   price: 4,
-  # user_id: rand(1..4)
+  user_id: ana.id
 )
 Game.create(
   name: "Soulcalibur",
   platform: 'Dreamcast',
   picture: 'https://assets-prd.ignimgs.com/2021/12/14/soulcalibur-1639521545469.jpg',
   price: 4,
-  # user_id: rand(1..4)
+  user_id: bob.id
 )
 Game.create(
   name: "Super Mario Galaxy",
   platform: 'Wii',
   picture: 'https://assets-prd.ignimgs.com/2020/09/04/super-mario-galaxy-1-button-1599258841533.jpg',
   price: 5,
-  # user_id: rand(1..4)
+  user_id: bob.id
 )
 Game.create(
   name: "Red Dead Redemption 2",
   platform: 'Xbox One',
   picture: 'https://assets1.ignimgs.com/2016/10/18/red-dead-redemption-2-buttonjpg-f9ad35.jpg',
   price: 5,
-  # user_id: rand(1..4)
+  user_id: bob.id
 )
 Game.create(
   name: "Perfect Dark",
   platform: 'Nintendo 64',
   picture: 'https://assets-prd.ignimgs.com/2020/09/04/perfect-dark-64-button-orig-1599255765856.jpg',
   price: 3,
-  # user_id: rand(1..4)
+  user_id: jon.id
 )
 Game.create(
   name: "Super Mario Bros",
   platform: 'Nintendo',
   picture: 'https://assets-prd.ignimgs.com/2022/01/08/smb-nesart-1641603921866.jpg',
   price: 3,
-  # user_id: rand(1..4)
+  user_id: jon.id
 )
 Game.create(
   name: "The Last Of Us",
   platform: 'Playstation',
   picture: 'https://assets-prd.ignimgs.com/2021/12/07/tlou-1638842419547.jpg',
   price: 5,
-  # user_id: rand(1..4)
+  user_id: jon.id
 )
 Game.create(
   name: "Mega Man",
   platform: 'Game Gear',
   picture: 'https://assets-prd.ignimgs.com/2022/04/05/megamangg-1649185884153.jpg',
   price: 3,
-  # user_id: rand(1..4)
+  user_id: jon.id
 )
 puts "10 games created!"
