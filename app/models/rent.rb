13868ea37ce6228a2fef670cd9786ba5dfa91178
Bruno Leote <b.leote@gmail.com>
2023-03-03 15:42:14 +0100

    all forms updated and fixed footer

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,6 +2,9 @@ class Rent < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
   validates :game_id, :uniqueness => { :scope => :user_id }
   enum status: %i[pending confirmed canceled]
 end
